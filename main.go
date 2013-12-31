d01d7b0ac35f948e9917ca49324a5069fa72cafc
Dominik Honnef <dominikh@fork-bomb.org>
2013-12-31 07:34:31 +0100

    do not detach DestroyNotify handler on unmap

--- main.go
+++ main.go
@@ -558,6 +558,8 @@ func (win *Window) UnmapNotify(xu *xgbutil.XUtil, ev xevent.UnmapNotifyEvent) {
 	LogWindowEvent(win, "Unmapping")
 	win.Mapped = false
 	win.Detach()
+	// Reattach DestroyNotify, because we still need that one
+	xevent.DestroyNotifyFun(win.DestroyNotify).Connect(win.wm.X, win.Id)
 	win.State = icccm.StateIconic
 	should(icccm.WmStateSet(win.wm.X, win.Id, &icccm.WmState{State: uint(win.State)}))
 }
