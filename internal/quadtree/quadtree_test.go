71d6fa7070e96931024e86e80ff9b009e90aa5e1
Dominik Honnef <dominik@honnef.co>
2016-08-26 09:17:25 +0200

    Add benchmarks for quadtree

--- internal/quadtree/quadtree_test.go
+++ internal/quadtree/quadtree_test.go
@@ -40,3 +40,49 @@ func TestTree(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkConstruction(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		q := New(3840, 2160)
+		q.Set(Region{0, 0, 3488, 1638}, int(48234500))
+		q.Set(Region{1413, 952, 712, 905}, int(37748740))
+		q.Set(Region{1600, 751, 2088, 1301}, int(20971521))
+		q.Set(Region{0, 0, 1944, 1004}, int(31457286))
+		q.Set(Region{343, 338, 804, 484}, int(58720262))
+		q.Set(Region{2448, 1213, 1284, 544}, int(50331654))
+		q.Set(Region{0, 0, 1204, 584}, int(33554438))
+		q.Set(Region{2584, 11, 1254, 684}, int(44040198))
+		q.Set(Region{2372, 1188, 1374, 784}, int(29360134))
+		q.Set(Region{2364, 14, 1474, 444}, int(39845894))
+		q.Set(Region{290, 1497, 804, 484}, int(56623110))
+		q.Set(Region{2652, 997, 1774, 644}, int(35651590))
+		q.Set(Region{0, 1614, 1444, 544}, int(16777222))
+		q.Set(Region{1362, 290, 1653, 1260}, int(20971577))
+		q.Set(Region{1228, 352, 1840, 1500}, int(12582932))
+		q.Set(Region{2102, 709, 1424, 1024}, int(67108870))
+	}
+}
+
+func BenchmarkRetrieve(b *testing.B) {
+	q := New(3840, 2160)
+	q.Set(Region{0, 0, 3488, 1638}, int(48234500))
+	q.Set(Region{1413, 952, 712, 905}, int(37748740))
+	q.Set(Region{1600, 751, 2088, 1301}, int(20971521))
+	q.Set(Region{0, 0, 1944, 1004}, int(31457286))
+	q.Set(Region{343, 338, 804, 484}, int(58720262))
+	q.Set(Region{2448, 1213, 1284, 544}, int(50331654))
+	q.Set(Region{0, 0, 1204, 584}, int(33554438))
+	q.Set(Region{2584, 11, 1254, 684}, int(44040198))
+	q.Set(Region{2372, 1188, 1374, 784}, int(29360134))
+	q.Set(Region{2364, 14, 1474, 444}, int(39845894))
+	q.Set(Region{290, 1497, 804, 484}, int(56623110))
+	q.Set(Region{2652, 997, 1774, 644}, int(35651590))
+	q.Set(Region{0, 1614, 1444, 544}, int(16777222))
+	q.Set(Region{1362, 290, 1653, 1260}, int(20971577))
+	q.Set(Region{1228, 352, 1840, 1500}, int(12582932))
+	q.Set(Region{2102, 709, 1424, 1024}, int(67108870))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		q.Get(3000, 500)
+	}
+}
